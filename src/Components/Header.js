bc0db3851e4a3d6cf44809befa452042dc200fe0
Nuo (Tony) Chen <chen2886@purdue.edu>
2021-04-25 17:01:15 -0400

    Updated speed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -28,7 +28,7 @@ class Header extends Component {
             {arr.map((item) => (
               <>
                 {item}
-                <Typing.Speed ms={25} />
+                <Typing.Speed ms={35} />
                 <Typing.Backspace count={18} delay={2000} />
                 <Typing.Speed ms={50} />
               </>
